0cf7fa31765a1f746f55e97daca70854e2fddee5
MaheshFiles <vmahesh359@gmail.com>
2023-05-27 17:24:57 +0530

    added

---
+++ src/com/mahesh/automation/corejava/Hardwork222.java
@@ -0,0 +1,14 @@
+package com.mahesh.automation.corejava;
+
+public class Hardwork2 {
+public static void main(String[] args) {
+	System.out.println("hardwork = job");
+
+System.out.println("hardwork = job");
+System.out.println("hardwork = job");
+
+	System.out.println("hardwork = jobsss");
+	
+
+}
+}
