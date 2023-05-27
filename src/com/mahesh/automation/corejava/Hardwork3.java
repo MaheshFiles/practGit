e30bfc7155340defae7b4b997c0feda6c3cd80e0
MaheshFiles <vmahesh359@gmail.com>
2023-05-27 13:36:53 +0530

    added new files by cloner

--- src/com/mahesh/automation/corejava/Hardwork3.java
+++ src/com/mahesh/automation/corejava/Hardwork3.java
@@ -3,6 +3,7 @@ package com.mahesh.automation.corejava;
 public class Hardwork3 {
 public static void main(String[] args) {
 	System.out.println("hello mahesh");
+System.out.println("hello mokshith");
 }
 
 }
---
+++ src/com/mahesh/automation/selenium/PractSE - Copy.java
@@ -0,0 +1,7 @@
+package com.mahesh.automation.selenium;
+
+public class PractSE {
+public static void main(String[] args) {
+	System.out.println("hello selenium");
+}
+}
